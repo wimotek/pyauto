38becaf7ca48d7299bb643872790718c125b61d8
wimotek <richard_wmf@163.com>
2021-03-05 23:46:12 +0800

    Update pyauto.py

--- pyauto.py
+++ pyauto.py
@@ -1,6 +1,20 @@
-import urllib.request,re
+import urllib.request,re,requests
 from urllib.parse import urlencode
 from http import cookiejar
 
+serverJ = process.env.PUSH_KEY;
 
-print ('test3')
+
+subject='测试'
+message='from github'
+result=sendNotify(subject,message)
+print (result)
+
+def sendNotify (text,desp):
+ api = "https://sc.ftqq.com/${serverJ}.send"
+ data = {
+   "text":text,
+   "desp":desp
+ }
+ req = requests.post(api,data = data)
+return req
